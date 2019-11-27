2bc2a98f682b47942521f1fa5ae7e922babe6723
Vadym Borovok <vadym.borovok@gmail.com>
2019-11-27 09:30:15 +0200

    use JWT js library for improve token

--- DatatingApp-SPA/src/app/nav/nav.component.ts
+++ DatatingApp-SPA/src/app/nav/nav.component.ts
@@ -21,8 +21,7 @@ export class NavComponent implements OnInit {
     });
   }
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
   logout() {
     localStorage.removeItem('token');
