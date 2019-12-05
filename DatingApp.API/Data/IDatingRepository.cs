7c47371d71af7e9f473607d20c844ac1dff95f50
Vadym Borovok <vadym.borovok@gmail.com>
2019-12-05 08:51:03 +0200

    bug fix

--- DatingApp.API/Data/IDatingRepository.cs
+++ DatingApp.API/Data/IDatingRepository.cs
@@ -1,15 +1,15 @@
-using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using DatingApp.API.Models;
 
 namespace DatingApp.API.Data
 {
     public interface IDatingRepository
     {
-        void Add<T>(T entity) where T: class;
-        void Delete<T>(T entity) where T: class;
-        Task<bool> SaveAll();
-        Task<IEnumerable<User>> GetUsers();
-        Task<User> GetUser();
+         void Add<T>(T entity) where T: class;
+         void Delete<T>(T entity) where T: class;
+         Task<bool> SaveAll();
+         Task<IEnumerable<User>> GetUsers();
+         Task<User> GetUser(int id);
     }
 }
